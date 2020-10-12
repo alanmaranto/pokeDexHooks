479189a276d7dcd5a40867abdf834ebf044b55ff
alan <alanarostegui@hotmail.com>
2020-10-12 00:14:52 -0500

    refactor(card): change colors

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,31 +1,15 @@
 import React from "react";
 
 import "./style.css";
-import typeColors from "../../helpers/typeColors";
+import { typeColors, backgroundColors } from "../../helpers/typeColors";
 
 const Card = ({ pokemon }) => {
-  const colors = {
-    fire: "#FDDFDF",
-    grass: "#DEFDE0",
-    electric: "#FCF7DE",
-    water: "#DEF3FD",
-    ground: "#f4e7da",
-    rock: "#d5d5d4",
-    fairy: "#fceaff",
-    poison: "#98d7a5",
-    bug: "#f8d5a3",
-    dragon: "#97b3e6",
-    psychic: "#eaeda1",
-    flying: "#F5F5F5",
-    fighting: "#E6E0D4",
-    normal: "#F5F5F5",
-  };
-  const main_types = Object.keys(colors);
 
+  const main_types = Object.keys(backgroundColors);
   const poke_types = pokemon.types.map((type) => type.type.name);
   const type = main_types.find((type) => poke_types.indexOf(type) > -1);
   const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
-  const color = colors[type];
+  const color = backgroundColors[type];
 
   const CardStyle = {
     backgroundColor: color,
